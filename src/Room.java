c5ff139f4db8a8370d60c148bd5bc6eb40b0b109
bryanf <bryanf@iastate.edu>
2018-02-08 20:52:51 -0600

    began filling out the basic contents of the room class. It will have neighbors in 6 directions, an isFound trait, a monster list, and an item list.

--- src/Room.java
+++ src/Room.java
@@ -1,7 +1,23 @@
+import java.util.List;
+
 public class Room {
+	
+	private Room forward;
+	private Room backward;
+	private Room left;
+	private Room right;
+	private Room up; //these are going to go unused for now because I want to develop the 2-D aspect first
+	private Room down;
+	
+	private boolean isFound;
+	
+	//private List<Monster> monsters;
+	//private List<Item> items;
 
 	public Room() {
-		// TODO Auto-generated constructor stub
+		isFound = false;
+		//monsters = new ArrayList<Monster>();
+		//items = new ArrayList<Item>();
 	}
 
 }
